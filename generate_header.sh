17071673997e51bdd47de4dae3a58f393a35c985
skullmind <xujianhuaap@gmail.com>
2021-06-30 16:44:13 +0800

    feature:project structure restore

--- generate_header.sh
+++ generate_header.sh
@@ -4,4 +4,4 @@ read input
 ProjectFileDir=$(pwd)
 echo "${ProjectFileDir}/app/build/tmp/kotlin-classes/debug"
 /usr/java/jdk1.8.0_251/bin/javah -cp ${ProjectFileDir}/app/build/tmp/kotlin-classes/debug -jni \
-  -d ${ProjectFileDir}/app/src/main/cpp/include ${input}
+  -d ${ProjectFileDir}/app/src/main/jni/include ${input}
