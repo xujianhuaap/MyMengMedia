dc73e4138299fb35e35b62c370d1908fb2485bee
xujianhua <1091041307@qq.com>
2022-07-18 16:28:26 +0800

    feature:TransCodeActivity

--- app/src/main/java/cn/skullmind/mbp/transcode/TransCodeActivity.kt
+++ app/src/main/java/cn/skullmind/mbp/transcode/TransCodeActivity.kt
@@ -1,14 +1,25 @@
 package cn.skullmind.mbp.transcode
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.app.AlertDialog
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import cn.skullmind.mbp.R
 import cn.skullmind.mbp.audio.RecordAudioAdapter
 import cn.skullmind.mbp.utils.getFileName
@@ -16,27 +27,68 @@ import cn.skullmind.mbp.utils.getRecordAudioFiles
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import java.io.File
+import java.util.*
 
 fun startTransCodeActivity(context: Context) {
     context.startActivity(Intent(context, TransCodeActivity::class.java))
 }
 
 class TransCodeActivity : AppCompatActivity() {
-    private val transcodeCallBack = object:TranscodeCallBack{
-        override fun transCodeSuccess() {
-            TODO("Not yet implemented")
-        }
+    private lateinit var tvFilterStatus:TextView
+    private lateinit var tvFilterInput:TextView
+    private lateinit var recFilterTranscodeResult:RecyclerView
+    private val transCodeAdapter = TransCodeAdapter()
 
-        override fun transCodeFailure() {
-            TODO("Not yet implemented")
-        }
-    }
-    private var selectFile: File? = null
+    private lateinit var vm:TranscodeViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_trans_code)
-        selectTranscodeFile()
 
+        initView()
+        initVm()
+        selectTranscodeFile()
+    }
+
+    private fun initVm() {
+        vm = ViewModelProvider(this)[TranscodeViewModel::class.java]
+        vm.getSelectFile().observe(this){
+            it?.run {
+                tvFilterInput.text = this.name
+            }
+        }
+
+        vm.getTranscodeStatus().observe(this){status ->
+            if(status == 0){
+                tvFilterStatus.text = "转码成功"
+            }else {
+                tvFilterStatus.text = "转码失败"
+            }
+
+        }
+
+        vm.getTranscodeResultFiles().observe(this){
+            transCodeAdapter.setData(Optional.ofNullable(it).orElse(ArrayList()))
+        }
+
+    }
+
+
+
+    private fun initView() {
+        tvFilterStatus = findViewById(R.id.tv_filter_status)
+        tvFilterStatus.setOnClickListener {
+            vm.refreshTranscodeFiles()
+        }
+        tvFilterInput = findViewById(R.id.tv_filter_input)
+        recFilterTranscodeResult = findViewById(R.id.rec_transcode_files)
+        recFilterTranscodeResult.setHasFixedSize(true)
+        recFilterTranscodeResult.layoutManager = LinearLayoutManager(this)
+        recFilterTranscodeResult.adapter = transCodeAdapter
+        val dividerItemDecoration = DividerItemDecoration(this, LinearLayout.VERTICAL).apply {
+            setDrawable(getDrawable(R.drawable.shape_divider_rect)!!)
+        }
+        recFilterTranscodeResult.addItemDecoration(dividerItemDecoration)
     }
 
     private fun selectTranscodeFile() {
@@ -45,7 +97,7 @@ class TransCodeActivity : AppCompatActivity() {
             val recordAudioAdapter = RecordAudioAdapter(recordAudioFiles)
             AlertDialog.Builder(this).setCancelable(false)
                 .setAdapter(recordAudioAdapter) { dialog, pos ->
-                    selectFile = recordAudioFiles[pos]
+                    vm.refreshSelectFile(recordAudioFiles[pos])
                     checkPermissions()
                     dialog.dismiss()
 
@@ -63,65 +115,19 @@ class TransCodeActivity : AppCompatActivity() {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if (requestCode == CODE_PERMISSION_STORAGE) {
             if (grantResults.all { it == PackageManager.PERMISSION_GRANTED }) {
-                startTranscode()
+                vm.startTranscode()
             }
         }
     }
 
     private fun checkPermissions() {
         if (permissions.all { checkSelfPermission(it) == PackageManager.PERMISSION_GRANTED }) {
-            startTranscode()
+            vm.startTranscode()
         } else {
             requestPermissions(permissions, CODE_PERMISSION_STORAGE)
         }
     }
 
-    private fun startTranscode() {
-        GlobalScope.launch {
-            selectFile?.run {
-
-                if(FFMpeg.transcode(getCmdStr(this)) == 0){
-                    transcodeCallBack.transCodeSuccess()
-                }else{
-                    transcodeCallBack.transCodeFailure()
-                }
-            }
-
-
-        }
-    }
-
-    private fun getCmdStr(inputFile: File): String {
-        val filterDir = getTranscodeResultDir(inputFile)
-        val lowOutPut = "${filterDir.absolutePath}/".plus(getFileName("low_", "wav"))
-        val highOutput = "${filterDir.absolutePath}/".plus(getFileName("high_", "wav"))
-
-        return FilterCmd(Filter.AcrossOver).getCmd(
-            arrayOf(
-                inputFile.absolutePath,
-                "1500",
-                lowOutPut,
-                highOutput,
-
-            )
-        )
-    }
-
-    private fun getTranscodeResultDir(inputFile: File): File {
-        val dirStr: String = inputFile.parentFile!!.absolutePath.plus("/Filter")
-        val filterDir = File(dirStr)
-        if (filterDir.exists()) {
-            filterDir.deleteRecursively()
-        }
-        filterDir.mkdir()
-        return filterDir
-    }
-
-    private fun getTranscodeResult():List<File>?{
-        return selectFile?.run {
-            getTranscodeResultDir(this).listFiles().filter { it.isFile }
-        }
-    }
 
 
     override fun onDestroy() {
@@ -138,7 +144,90 @@ class TransCodeActivity : AppCompatActivity() {
     }
 }
 
-interface TranscodeCallBack{
-    fun transCodeSuccess()
-    fun transCodeFailure()
+
+
+class TranscodeViewModel:ViewModel(){
+    private val selectFile  = MutableLiveData<File?>()
+    private val transcodeFiles = MutableLiveData<List<File>?>()
+    private val transcodeStatus = MutableLiveData<Int>()
+
+    fun getSelectFile() = selectFile
+    fun refreshSelectFile(file:File?){
+        selectFile.value = file
+    }
+    fun getTranscodeResultFiles() = transcodeFiles
+
+    fun getTranscodeStatus() = transcodeStatus
+
+     fun startTranscode() {
+        GlobalScope.launch {
+            selectFile.value?.run {
+                transcodeStatus.postValue(FFMpeg.transcode(getCmdStr(this)))
+                if(transcodeStatus.value == 0){
+                    transcodeFiles.postValue(getTranscodeResult())
+                }
+            }
+        }
+    }
+
+    private fun getCmdStr(inputFile: File): String {
+        val filterDir = getTranscodeResultDir(inputFile)
+        filterDir.listFiles()?.forEach { it.delete() }
+        val lowOutPut = "${filterDir.absolutePath}/".plus(getFileName("low_", "wav"))
+        val highOutput = "${filterDir.absolutePath}/".plus(getFileName("high_", "wav"))
+
+        return FilterCmd(Filter.AcrossOver).getCmd(
+            arrayOf(
+                inputFile.absolutePath,
+                "1500",
+                lowOutPut,
+                highOutput,
+
+                )
+        )
+    }
+
+
+    fun refreshTranscodeFiles(){
+        transcodeFiles.postValue(getTranscodeResult())
+    }
+    private fun getTranscodeResultDir(inputFile: File): File {
+        val dirStr: String = inputFile.parentFile!!.absolutePath.plus("/Filter")
+        val filterDir = File(dirStr)
+        if(!filterDir.exists()) filterDir.mkdir()
+        return filterDir
+    }
+
+     private fun getTranscodeResult():List<File>?{
+        return selectFile.value?.run {
+            getTranscodeResultDir(this).listFiles()?.filter { it.isFile }
+        }
+    }
+
+}
+
+class TransCodeAdapter:RecyclerView.Adapter<TransCodeViewHolder>(){
+    private val files = ArrayList<File>()
+    @SuppressLint("NotifyDataSetChanged")
+    fun setData(files:List<File>){
+        this.files.clear()
+        this.files.addAll(files)
+        notifyDataSetChanged()
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TransCodeViewHolder {
+        val rootView = LayoutInflater.from(parent.context).inflate(R.layout.item_ffmpeg_transcode_file,parent,false)
+        return TransCodeViewHolder(rootView)
+    }
+
+    override fun onBindViewHolder(holder: TransCodeViewHolder, position: Int) {
+        holder.bind(files[position])
+    }
+
+    override fun getItemCount(): Int  = files.size
+}
+
+class TransCodeViewHolder(rootView: View):RecyclerView.ViewHolder(rootView){
+    fun bind(file:File){
+        itemView.findViewById<TextView>(R.id.tv_transcode_file).text = file.name
+    }
 }
